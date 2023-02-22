f8d27f13d2691dc8e55860ad9775f3bf2e076c20
user B <21020658@vnu.edu.vn>
2023-02-23 06:18:09 +0700

    css(togglelist), js(deleteTask, countTask)

--- app.js
+++ app.js
@@ -1,30 +1,110 @@
-const button = document.getElementById("favoritesButton");
-const content = document.getElementById("favorites");
+const buttonFavorites = document.getElementById("favoritesButton");
+const contentFavorites = document.getElementById("favorites");
 
-button.onclick = function() {
-    if (content.className == "open") {
-        content.className = "";
-        button.innerHTML = "Close Favorites";
+buttonFavorites.onclick = function() {
+    if (contentFavorites.className == "open") {
+        contentFavorites.className = "";
+        buttonFavorites.innerHTML = "Close Favorites";
     }
     else {
-        content.className = "open";
-        button.innerHTML = "Open Favorites";
+        contentFavorites.className = "open";
+        buttonFavorites.innerHTML = "Open Favorites";
     }
 }
 
-// const rightButton = document.getElementsByClassName("add_task_button");
-// const rightContent = document.getElementsByClassName("add_task");
+const buttonPersonal = document.getElementById("personalButton");
+const contentPersonal = document.getElementById("personalSpace");
 
-const taskInput = document.querySelector('.add_task');
-const taskButton = document.querySelector('.add_task_button');
-const list = document.querySelector(".task_list");
+buttonPersonal.addEventListener("click", () => {
+    if (contentPersonal.className == "open") {
+        contentPersonal.className = "";
+        buttonPersonal.innerHTML = "Close Personal";
+    }
+    else {
+        contentPersonal.className = "open";
+        buttonPersonal.innerHTML = "Open Personal";
+    }
+})
 
-taskButton = addEventListener('click', addTask);
+const buttonWork = document.getElementById("workButton");
+const contentWork = document.getElementById("workSpace");
 
+buttonWork.addEventListener("click", () => {
+    if (contentWork.className == "open") {
+        contentWork.className = "";
+        buttonWork.innerHTML = "Close Personal";
+    }
+    else {
+        contentWork.className = "open";
+        buttonWork.innerHTML = "Open Personal";
+    }
+})
+
+
+let taskInput = document.querySelector('.add_task');
+let taskButton = document.querySelector('.add_task_button');
+let list = document.querySelector(".task_list");
+
+taskButton.addEventListener('click', addTask);
+taskInput.addEventListener('keypress', addTaskByEnter);
+function addTaskByEnter(e) {
+    if (e.keyCode == 13) {
+        addTask();
+    }
+}
 function addTask (e) {
+    console.log(e);
     const li = document.createElement("li");
     li.className = "task_item";
     li.textContent = taskInput.value;
+    if (li.textContent == null || li.textContent == "") {
+        return;
+    }
     list.appendChild(li);
     taskInput.value = '';
+    li.addEventListener('click', () => {
+        list.removeChild(li);console.log(list.childElementCount);
+        updateTodayTasks();
+    })
+    updateTodayTasks();
 }
+
+let countToday = document.getElementById('today_count');
+
+function updateTodayTasks (e) {
+   countToday = list.childElementCount; 
+   console.log(countToday);
+   document.getElementById('today_count').innerHTML = countToday;
+}
+// let deleteButton = document.querySelector(".remove_button");
+// deleteButton.addEventListener("click", deleteTask);
+// function deleteTask (e) {
+//     let task = document.querySelector(".task_item");
+//     if (task){
+//         task.addEventListener("click", check);
+//         alert("task is okay");
+//         console.log(task);
+//     }
+//     if (task == null) {
+//         console.log("nothing here");
+//     }
+// }
+
+// function check (e) {
+//     list.parentElement.removeChild(list.firstChild);
+//     e.preventDefault();
+// }
+// 
+// task.addEventListener("click", deleteTask);
+// function deleteTask(e) {
+//     list.remove(list.lastChild);
+//     console.log(54);
+// }
+
+
+
+// list.addEventListener("click", deleteTask);
+// function deleteTask(e) {
+//     const listLi = document.getElementsByClassName('task_list');
+//     listLi.remove();
+// }
